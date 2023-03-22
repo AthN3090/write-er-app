9802105a4f7f385c03418401f165c928965db470
AthN3090 <amandevrishi12@gmail.com>
2023-03-22 18:47:05 +0530

    favicon change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import { baseAPI, Bucket } from "../App";
+import { baseAPI} from "../App";
 import {login} from '../features/user'
 import logo from '../assets/logo.PNG'
 import edit from '../assets/edit.png'
