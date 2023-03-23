cc312bad5bf72144b59e29b410bcc1cbca220a97
AthN3090 <amandevrishi12@gmail.com>
2023-03-23 21:46:54 +0530

    sign up and login page height fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 
 function Login() {
   return (
-    <div className="flex flex-col h-full items-stretch justify-center">
+    <div className="flex flex-col items-stretch justify-center">
       <Navbar />
       <div className="flex justify-center bg-gray-100 items-center grow">
         <div className="">
